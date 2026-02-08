d6e52d440b8a22187addce8bb6b8ce95d217df97
BacarImane <imanebacar@outlook.fr>
2026-02-09 01:09:00 +0300

    debug render.

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 const morgan = require('morgan');
 const express = require("express");
 const mongoose = require('mongoose');
-
+const path = require('path'); 
 const app = express()
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(cors({
   origin: "http://localhost:3000",
@@ -26,4 +27,6 @@ app.use(express.json())
 const router = require('./routes/routes')
 app.use('/api',router)
 
-app.listen(5014,()=> console.log('Server Started 3000'));
+const PORT = process.env.PORT || 5014;
+
+app.listen(PORT,()=> console.log('Server Started 3000'));
